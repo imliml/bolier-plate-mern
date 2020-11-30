07ffe1a66f6733d1fcbbbc3f0680af1b327c9345
lsr <imlimr1232@gmail.com>
2020-11-30 17:46:17 +0900

    로그인, auth라우트

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
+const { auth } = require("./model/User");
 const { User } = require("./model/User");
 
 // application/x-www-form-urlencoded
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -26,7 +27,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원가입 할때 필요한 정보들을 client에 가져오면 그것들은 데이터 베이스에 넣어줌
   const user = new User(req.body);
 
@@ -35,6 +36,51 @@ app.post("/register", (req, res) => {
     return res.status(200).json({
       success: true,
     });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      // 비밀번호까지 맞다면 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토근을 쿠키에 저장
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 예기는 Authentication이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
